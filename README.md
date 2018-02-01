7890bda0a6d1f3df0e3acfb3f05ed70e8d24c346
David Whitters <whitterd@mail.gvsu.edu>
2018-02-01 11:02:54 -0500

    Implemented main thread functionality.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MultithreadedServer
 CIS 452 Blocking Multi-Threaded Server Lab 4
+
+## Compilation
+- pthread library must be linked.
--- server.c
+++ server.c
@@ -1,3 +1,11 @@
+/******************************************************************
+ * @authors Jonah Bukowsky, David Whitters
+ * @date 2/1/18
+ *
+ * CIS 452
+ * Dr. Dulimarta
+ ****************************************************************/
+
 #include <pthread.h>
 #include <stdio.h>
 #include <errno.h>
@@ -5,24 +13,37 @@
 #include <stdlib.h>
 #include <string.h>
 
+/** The maximum number of character that will be accepted from the user. */
+#define MAX_NUM_INPUT_CHARS 1028
+
 void *worker_thread(void *arg);
 
 int main()
 {
-    pthread_t thread1;
-    void *result1;
+    /* Not used, handle to the latest thread created. */
+    pthread_t latest_thread;
     int status;
 
-	if ((status = pthread_create(&thread1, NULL, worker_thread, NULL)) != 0) {
-	    fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
-	    exit(EXIT_FAILURE);
-    }
+    /* Holds the pointer to the user input. */
+    char * str;
 
-    //join with the threads(wait for them to terminate
-    // get their return vals
-    if ((status = pthread_join(thread1, &result1)) != 0) {
-	    fprintf(stderr, "join error %d:%s\n", status, strerror(status));
-	    exit(1);
+    /* Seed the random number generator. */
+    srand(getpid());
+
+    while(1)
+    {
+        /* Allocate memory for the user input. */
+        str = (char *)malloc(MAX_NUM_INPUT_CHARS);
+        /* Prompt the user for input. */
+        printf("Please input a file name: ");
+        /* Store the user input. */
+        scanf("%s", str);
+
+        /* Spawn a worker thread with the user input. */
+        if ((status = pthread_create(&latest_thread, NULL, worker_thread, str)) != 0) {
+            fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
+            exit(EXIT_FAILURE);
+        }
     }
 
     return 0;
@@ -31,6 +52,5 @@ int main()
 void *worker_thread(void *arg)
 {
 
-    
     return NULL;
 }
